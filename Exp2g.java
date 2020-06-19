f9269541297d5348a78601216278c1ee267e7270
rishabhs-s <47741140+rishabhs-s@users.noreply.github.com>
2020-06-19 23:47:00 +0530

    exp 2g update

--- Exp2g.java
+++ Exp2g.java
@@ -1,3 +1,29 @@
 /*WRITE A PROGRAM TO ACCEPT THREE DIGITS (I.E. 0 - 9) AND PRINT ALL ITS POSSIBLE COMBINATIONS.  
 (FOR EXAMPLE IF THE THREE DIGITS ARE 1, 2, 3 THAN ALL POSSIBLE COMBINATIONS ARE : 123, 132, 
 213, 231, 312, 321.)*/
+
+import java.util.*;
+public class Exp2g{
+public static void main(String args[])
+{
+Scanner pc = new Scanner(System.in);
+int a[] = new  int[3] ;
+System.out.println("Enter 3 numbers");
+for (int i=0; i<3; i++)
+a[i] = pc.nextInt();
+
+for (int x = 0; x < 3; x++)
+{
+for (int y = 0; y < 3; y++) 
+{
+for (int z = 0; z < 3; z++) 
+{
+if (x != y && y != z && z != x) 
+{
+System.out.println(a[x] + "" + a[y] + ""+ a[z]);
+}
+}
+}
+}
+}
+}
