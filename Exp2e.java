08a7ed3f64e18079dce23ba0a651a98e18ae4208
rishabhs-s <47741140+rishabhs-s@users.noreply.github.com>
2020-06-19 23:01:56 +0530

    Exp2e updated

---
+++ Exp2e.java
@@ -0,0 +1,14 @@
+// Q	Write a program using classes and object in java.
+	public class Exp2e{
+	int x=45 , y=88;
+	public static void main(String args[])
+	{
+	Exp2e obj1 = new Exp2e();
+
+	if(obj1.x < obj1.y)
+	System.out.println(obj1.x + " is lesser than" + obj1.y);
+
+	else 
+	System.out.println(obj1.x +" " + "is greater than" + " "+ obj1.y);
+
+	}}
